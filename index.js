b8c929f29ef8e79370856e3b3f398ea85f8af3fe
Yash Agarwal <yashagar0412@gmail.com>
2021-06-27 00:03:37 +0530

    added bot code

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 require('dotenv').config();
 const Discord = require('discord.js');
+const axios = require('axios');
+
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+const wazirXAPI = 'https://api.wazirx.com/api/v2/tickers';
+
+const generalMessage = `
+Hello From Crypto Bot!!
+Send **!crypto help** for valid commands.
+`;
+
+const instructionsMsg = `
+Send **!crypto <Cryptocurrency Base Unit>** to receive current price.
+Examples: 
+**!crypto btc** for Bitcoin
+**!crypto eth** for Ethereum
+**!crypto matic** for Matic(Polygon)
+`;
 
 bot.login(TOKEN);
 
@@ -9,17 +25,42 @@ bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
+bot.on('message', async (msg) => {
+  if (msg.content.startsWith('!crypto')) {
+    const commandArray = msg.content.split(' ');
+    if (commandArray.length < 2) {
+      msg.reply(generalMessage);
+      return;
+    }
+    const command = commandArray[1];
+    if (command === 'help') {
+      msg.reply(instructionsMsg);
+      return;
+    }
+    try {
+      const price = await fetchBTCInfo(`${command.toLowerCase()}inr`);
+      if (price)
+        msg.reply(
+          `${price.name} - ₹${price.last} at ${new Date().toLocaleString()}`
+        );
+      else {
+        msg.reply(`Invalid Crypto Name ${command}`);
+      }
+    } catch (err) {
+      msg.reply(`Error while fetching Price`);
     }
   }
 });
+
+function fetchBTCInfo(currencyName) {
+  return axios
+    .get(wazirXAPI)
+    .then((res) => {
+      const price = res.data[currencyName];
+      return price;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+}
